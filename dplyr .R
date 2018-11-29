a7f518afe18c208e5506a7983f9873e6b37fba9b
shannonsamuel27 <ShannonSamuel@Shannons-MacBook-Pro.local>
2018-11-29 14:39:28 +1100

    afternoon tea

--- dplyr .R
+++ dplyr .R
@@ -25,3 +25,25 @@ interviews %>% filter(memb_assoc =="yes") %>% select(affect_conflicts, liv_count
 
 #mutate
 interviews<- interviews %>% mutate(people_per_room = no_membrs/rooms)
+
+mean(interviews$no_membrs)
+
+interviews %>% group_by(village)%>% summarise(mean_no_membrs =mean(no_membrs))
+
+interviews %>% group_by(village) %>% filter(memb_assoc == "yes") %>% 
+  summarise(mean_no_membrs =mean(no_membrs))
+
+interviews %>% group_by(village, memb_assoc) %>% summarise(mean_no_membrs =mean (no_membrs))
+
+interviews %>% count((village))
+interviews %>% count(village, sort = TRUE)                                
+
+#interviews<- interviews %>% mutate(no_membrs = no_membrs*no_meals)
+#mean(interviews$no_meals)                           
+
+interviews %>% group_by(village) %>% 
+  summarise(mean_membrs = mean (no_membrs), 
+            min_membrs=min(no_membrs), 
+            max_membrs = max (no_membrs)
+            , n=n())
+                                              
