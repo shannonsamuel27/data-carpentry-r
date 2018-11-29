dfe1e440715f7c4472e1ae4888bbc475f5512c9a
shannonsamuel27 <ShannonSamuel@Shannons-MacBook-Pro.local>
2018-11-29 15:40:21 +1100

    plotting plotting plotting

--- dplyr .R
+++ dplyr .R
@@ -50,5 +50,28 @@ interviews %>% group_by(village) %>%
 
 #reshaping 
 
-View(interviews %>% mutate(wall_type_logical = TRUE) %>%
-  spread(key = respondent_wall_type, value = wall_type_logical )
+(interviews %>% mutate(wall_type_logical = TRUE) %>%
+  spread(key = respondent_wall_type, value = wall_type_logical, fill = FALSE ))
+  
+interviews <- interviews %>% gather (key = respondent_wall_type, value = "wall_type_logical", burntbricks:sunbricks)
+
+
+
+#prepare
+
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
+interviews_plotting <- interviews %>%
+  mutate(split_items = strsplit(items_owned, ";")) %>%
+  unnest() %>%
+  mutate(items_owned_logical = TRUE) %>%
+  spread (key=split_items, value = items_owned_logical, fill = FALSE) %>%
+  rename(no_listed_items ='<NA>') %>%
+  mutate(split_months=strsplit(months_lack_food, ";")) %>%
+  unnest() %>%
+  mutate(months_lack_food_logical=TRUE) %>%
+  spread(key = split_months, value=months_lack_food_logical, fill = FALSE) %>%
+  mutate(numer_month_lack_food= rowSums(select(. , Apr:Sept))) %>%
+  mutate(number_items=rowSums(select(., bicycle:television)))
+
+View(interviews_plotting)
+write_csv(interviews_plotting, path="data_output/interviews_plotting.csv")
