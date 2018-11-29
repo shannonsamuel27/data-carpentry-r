8666471a0a92c74bcb41ad56e76f05b77fdafedd
shannonsamuel27 <ShannonSamuel@Shannons-MacBook-Pro.local>
2018-11-29 13:05:42 +1100

    post lunch committ

--- Github 2.R
+++ Github 2.R
@@ -66,3 +66,52 @@ interviews[ceiling(n_row/2),]
 
 
 interviews[-(7:nrow(interviews)),]
+
+
+
+#Factors: Priority, Sex ( male: female). Character variable, if you use a factor R will help you more. 
+
+respondent_floor_type<- factor(c("earth","cement","cement","earth"))
+levels (respondent_floor_type)
+nlevels(respondent_floor_type)
+
+respondent_floor_type
+respondent_floor_type<-factor(respondent_floor_type, levels =c("earth", "cement"))
+
+levels(respondent_floor_type)[2]
+
+levels(respondent_floor_type)[2]<-"brick"
+respondent_floor_type
+
+as.character(respondent_floor_type)
+
+year_fct<-factor(c(1990, 1983, 1977, 1998, 1990))
+year_fct
+as.numeric(year_fct)
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+affect_conflicts<-interviews$affect_conflicts
+affect_conflicts<- interviews[[, "affect_conflcits"]] #achieve the same thing 
+affect_conflicts<-factor(affect_conflicts)
+affect_conflicts
+
+plot(affect_conflicts)
+
+affect_conflicts <- interviews$affect_conflicts
+affect_conflicts[is.na(affect_conflicts)]<-"undetermined"
+affect_conflicts
+affect_conflicts<-factor(affect_conflicts)
+
+plot(affect_conflicts)
+
+
+
+
+levels(affect_conflicts)[2]<-'more than once'
+levels(affect_conflicts)
+affect_conflicts<- factor(affect_conflicts,levels = c("never","once" ,"more than once","frequently", "undetermined"))
+plot(affect_conflicts,ylim=c(0,50))
+box ()
+
+
