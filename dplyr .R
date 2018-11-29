7fcc7a1e272da48201e83bfcf67fc4aeb0872d55
shannonsamuel27 <ShannonSamuel@Shannons-MacBook-Pro.local>
2018-11-29 13:46:40 +1100

    pipes

--- dplyr .R
+++ dplyr .R
@@ -5,3 +5,17 @@ interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
 
 select(interviews, village, no_membrs, years_liv)
 select(interviews, village:rooms)
+
+#filtering 
+filter(interviews, village=="God")
+filter(interviews, 1:nrow(interviews)%in% 1:10)
+
+interviews2<-filter(interviews, village=="God")
+interviews_god<-select(interviews2, no_membrs, years_liv)
+interviews_god
+
+interviews_god <-select(filter(interviews, village =="God"), no_membrs, years_liv)
+
+interviws_god<-interviews%>% filter(village=="God")%>% 
+  select(no_membrs, years_liv)                 
+                 
